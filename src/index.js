2b8b98c25b4fa9f7a1402228f5d4a954541384f8
jakozbek <16109785+jakozbek@users.noreply.github.com>
2025-05-14 16:43:14 -0500

    Updates formatting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-const { polygon: turfPolygon, lineString: turfLine, point: turfPoint } = require("@turf/helpers")
-const { default: turfBboxPolygon } = require("@turf/bbox-polygon")
-const { default: turfBbox } = require("@turf/bbox")
-const { default: turfEnvelope } = require("@turf/envelope")
-const { default: turfIntersect } = require("@turf/intersect")
-const { default: turfBooleanOverlap } = require("@turf/boolean-overlap")
-const { default: turfBooleanWithin } = require("@turf/boolean-within")
-const { default: turfBooleanPointInPolygon } = require("@turf/boolean-point-in-polygon")
-const { default: turfLineSplit } = require("@turf/line-split")
-const { default: turfArea } = require("@turf/area")
-const ngeohash = require("ngeohash")
-const Stream = require("stream")
+const {
+  polygon: turfPolygon,
+  lineString: turfLine,
+  point: turfPoint,
+} = require("@turf/helpers");
+const { default: turfBboxPolygon } = require("@turf/bbox-polygon");
+const { default: turfBbox } = require("@turf/bbox");
+const { default: turfEnvelope } = require("@turf/envelope");
+const { default: turfIntersect } = require("@turf/intersect");
+const { default: turfBooleanOverlap } = require("@turf/boolean-overlap");
+const { default: turfBooleanWithin } = require("@turf/boolean-within");
+const {
+  default: turfBooleanPointInPolygon,
+} = require("@turf/boolean-point-in-polygon");
+const { default: turfLineSplit } = require("@turf/line-split");
+const { default: turfArea } = require("@turf/area");
+const ngeohash = require("ngeohash");
+const Stream = require("stream");
 
 const {
   isPoint,
@@ -18,7 +24,7 @@ const {
   switchBbox,
   allRectangleEdgesWithin,
   extractCoordinatesFromGeoJSON,
-} = require("./helpers")
+} = require("./helpers");
 
 const defaultOptions = {
   precision: 6,
@@ -26,99 +32,107 @@ const defaultOptions = {
   minIntersect: 0,
   customWriter: null,
   allowDuplicates: true,
-}
+};
 
 async function shape2geohash(shapes, options = {}) {
-  options = { ...defaultOptions, ...options } // overwrite default options
+  options = { ...defaultOptions, ...options }; // overwrite default options
 
-  let allShapes = null
+  let allShapes = null;
   if (Array.isArray(shapes)) {
     // The input is either an array of polygon coordinates or a list of polygons
-    allShapes = isMulti(shapes) ? shapes : [shapes] // Make sure allShapes is always an array of shapes
+    allShapes = isMulti(shapes) ? shapes : [shapes]; // Make sure allShapes is always an array of shapes
   } else {
-    allShapes = extractCoordinatesFromGeoJSON(shapes)
+    allShapes = extractCoordinatesFromGeoJSON(shapes);
   }
 
-  let allGeohashes = []
+  let allGeohashes = [];
 
   if (!options.allowDuplicates) {
-    allGeohashes = new Set()
+    allGeohashes = new Set();
   }
 
-  const addGeohashes = geohashes => {
+  const addGeohashes = (geohashes) => {
     if (!options.allowDuplicates) {
-      geohashes.forEach(gh => allGeohashes.add(gh))
-      return
+      geohashes.forEach((gh) => allGeohashes.add(gh));
+      return;
     }
-    allGeohashes.push(...geohashes)
-  }
+    allGeohashes.push(...geohashes);
+  };
 
-  const allShapePromises = allShapes.map(shape => {
+  const allShapePromises = allShapes.map((shape) => {
     return new Promise((resolve, reject) => {
       if (isPoint(shape) && options.customWriter === null) {
         // Optimization for points. No need for streams.
-        addGeohashes([ngeohash.encode(...shape.reverse(), options.precision)])
-        resolve()
-        return
+        addGeohashes([ngeohash.encode(...shape.reverse(), options.precision)]);
+        resolve();
+        return;
       }
 
-      const geohashStream = new GeohashStream(shape, options)
+      const geohashStream = new GeohashStream(shape, options);
 
       const writer = new Stream.Writable({
         objectMode: true,
         write: (rowGeohashes, enc, callback) => {
-          addGeohashes(rowGeohashes)
-          callback()
+          addGeohashes(rowGeohashes);
+          callback();
         },
-      })
+      });
 
       if (options.customWriter) {
-        geohashStream.pipe(options.customWriter)
+        geohashStream.pipe(options.customWriter);
       } else {
-        geohashStream.pipe(writer) // Kick off the stream
+        geohashStream.pipe(writer); // Kick off the stream
       }
 
       geohashStream.on("end", () => {
-        resolve()
-      })
-    })
-  })
+        resolve();
+      });
+    });
+  });
 
-  return Promise.all(allShapePromises).then(() => Array.from(allGeohashes))
+  return Promise.all(allShapePromises).then(() => Array.from(allGeohashes));
 }
 
 class GeohashStream extends Stream.Readable {
   constructor(shapeCoordinates, options) {
-    super({ objectMode: true })
+    super({ objectMode: true });
 
-    this.options = options
+    this.options = options;
 
-    this.shapeIsPoint = isPoint(shapeCoordinates)
+    this.shapeIsPoint = isPoint(shapeCoordinates);
     if (this.shapeIsPoint) {
-      this.pointCoordinates = shapeCoordinates
-      return
+      this.pointCoordinates = shapeCoordinates;
+      return;
     }
 
     this.originalShape = isLine(shapeCoordinates)
       ? turfLine(shapeCoordinates)
-      : turfPolygon(shapeCoordinates)
+      : turfPolygon(shapeCoordinates);
 
     // [minX, minY, maxX, maxY]
-    const originalEnvelopeBbox = turfBbox(turfEnvelope(this.originalShape))
+    const originalEnvelopeBbox = turfBbox(turfEnvelope(this.originalShape));
 
     // [minX, minY, maxX, maxY]
     const topLeftGeohashBbox = switchBbox(
       ngeohash.decode_bbox(
-        ngeohash.encode(originalEnvelopeBbox[3], originalEnvelopeBbox[0], this.options.precision)
-      )
-    )
+        ngeohash.encode(
+          originalEnvelopeBbox[3],
+          originalEnvelopeBbox[0],
+          this.options.precision,
+        ),
+      ),
+    );
 
     // [minX, minY, maxX, maxY]
     const bottomRightGeohashBbox = switchBbox(
       ngeohash.decode_bbox(
-        ngeohash.encode(originalEnvelopeBbox[1], originalEnvelopeBbox[2], this.options.precision)
-      )
-    )
+        ngeohash.encode(
+          originalEnvelopeBbox[1],
+          originalEnvelopeBbox[2],
+          this.options.precision,
+        ),
+      ),
+    );
 
     // The extended geohash envelope covers the area from top left geohash until bottom right geohash
     // I use it instead of the original envelope because I want every row match the real geohash row
@@ -127,47 +141,54 @@ class GeohashStream extends Stream.Readable {
       bottomRightGeohashBbox[1],
       bottomRightGeohashBbox[2],
       topLeftGeohashBbox[3],
-    ]
+    ];
 
-    this.rowWidth = Math.abs(geohashEnvelopeBbox[2] - geohashEnvelopeBbox[0])
-    this.geohashHeight = Math.abs(topLeftGeohashBbox[3] - topLeftGeohashBbox[1])
+    this.rowWidth = Math.abs(geohashEnvelopeBbox[2] - geohashEnvelopeBbox[0]);
+    this.geohashHeight = Math.abs(
+      topLeftGeohashBbox[3] - topLeftGeohashBbox[1],
+    );
 
     // Current point is the top left corner of the extended geohash envelope
     // Traversing the polygon from top to bottom
-    this.currentPoint = [geohashEnvelopeBbox[0], geohashEnvelopeBbox[3]]
+    this.currentPoint = [geohashEnvelopeBbox[0], geohashEnvelopeBbox[3]];
 
     // Bottom border of the extended geohash envelope
-    this.bottomLimit = geohashEnvelopeBbox[1]
+    this.bottomLimit = geohashEnvelopeBbox[1];
 
     // The minimum shared area between the polygon and the geohash
     this.minIntersectArea =
-      this.options.minIntersect * turfArea(turfBboxPolygon(topLeftGeohashBbox))
+      this.options.minIntersect * turfArea(turfBboxPolygon(topLeftGeohashBbox));
 
     // Used in processRowSegment to keep track of how much area of the row
     // has been covered by the matching geohashes
     // Prevent duplicate geohashes
-    this.rowProgress = -Infinity
+    this.rowProgress = -Infinity;
   }
 
   _read(size) {
     if (this.shapeIsPoint) {
-      this.push([ngeohash.encode(...this.pointCoordinates.reverse(), this.options.precision)])
-      this.push(null)
-      return
+      this.push([
+        ngeohash.encode(
+          ...this.pointCoordinates.reverse(),
+          this.options.precision,
+        ),
+      ]);
+      this.push(null);
+      return;
     }
 
-    const rowGeohashes = this.processNextRow()
+    const rowGeohashes = this.processNextRow();
     if (rowGeohashes !== null) {
-      this.push(rowGeohashes) // Push data out of the stream
+      this.push(rowGeohashes); // Push data out of the stream
     } else {
-      this.push(null) // End the stream
+      this.push(null); // End the stream
     }
   }
 
   processNextRow() {
     if (this.currentPoint[1] <= this.bottomLimit) {
       // We have reached the bottom of the polygon
-      return null
+      return null;
     }
 
     // Calculate the row polygon
@@ -176,116 +197,131 @@ class GeohashStream extends Stream.Readable {
       this.currentPoint[1] - this.geohashHeight,
       this.currentPoint[0] + this.rowWidth,
       this.currentPoint[1],
-    ])
+    ]);
 
-    const geohashes = [] // Geohashes for this row
+    const geohashes = []; // Geohashes for this row
 
     if (this.options.hashMode === "envelope") {
-      geohashes.push(...this.processRowSegment(rowPolygon.geometry.coordinates))
+      geohashes.push(
+        ...this.processRowSegment(rowPolygon.geometry.coordinates),
+      );
     } else {
       if (this.originalShape.geometry.type === "LineString") {
-        const lineSegments = turfLineSplit(this.originalShape, rowPolygon).features
+        const lineSegments = turfLineSplit(
+          this.originalShape,
+          rowPolygon,
+        ).features;
 
         if (lineSegments.length === 0) {
           // Line is completely inside rowPolygon so just add the originalShape
-          lineSegments.push(this.originalShape)
+          lineSegments.push(this.originalShape);
         }
 
-        let evenPairs
-        const firstPointOfFirstSegment = lineSegments[0].geometry.coordinates[0]
+        let evenPairs;
+        const firstPointOfFirstSegment =
+          lineSegments[0].geometry.coordinates[0];
         if (turfBooleanPointInPolygon(firstPointOfFirstSegment, rowPolygon)) {
-          evenPairs = true
+          evenPairs = true;
         } else {
-          evenPairs = false
+          evenPairs = false;
         }
         // Filter for line segments that are inside the row polygon
         // Put an envelope around the segment and get geohashes
         lineSegments
           .filter((p, i) => (evenPairs ? i % 2 == 0 : i % 2 == 1))
-          .forEach(lineSegment => {
-            const lineSegmentEnvelope = turfEnvelope(lineSegment).geometry.coordinates
-            geohashes.push(...this.processRowSegment(lineSegmentEnvelope))
-          })
+          .forEach((lineSegment) => {
+            const lineSegmentEnvelope =
+              turfEnvelope(lineSegment).geometry.coordinates;
+            geohashes.push(...this.processRowSegment(lineSegmentEnvelope));
+          });
       } else {
         // Its a Polygon
         // Calculate the intersection between the row and the original polygon
-        const intersectionGeoJSON = turfIntersect(this.originalShape, rowPolygon)
+        const intersectionGeoJSON = turfIntersect(
+          this.originalShape,
+          rowPolygon,
+        );
         if (intersectionGeoJSON !== null) {
-          let coordinates = intersectionGeoJSON.geometry.coordinates
-          coordinates = isMulti(coordinates) ? coordinates : [coordinates]
+          let coordinates = intersectionGeoJSON.geometry.coordinates;
+          coordinates = isMulti(coordinates) ? coordinates : [coordinates];
 
           // Check every intersection part for geohashes
-          coordinates.forEach(polygon => {
-            geohashes.push(...this.processRowSegment(polygon))
-          })
+          coordinates.forEach((polygon) => {
+            geohashes.push(...this.processRowSegment(polygon));
+          });
         }
       }
     }
 
     // Move one row lower
-    this.currentPoint[1] -= this.geohashHeight
+    this.currentPoint[1] -= this.geohashHeight;
 
     // Reset rowProgress
-    this.rowProgress = -Infinity
+    this.rowProgress = -Infinity;
 
-    return geohashes
+    return geohashes;
   }
 
   // Returns all the geohashes that are within the current row
   processRowSegment(coordinates) {
     // Convert coordinates into polygon object
-    const segmentPolygon = turfPolygon(coordinates)
-    const envelopeBbox = turfBbox(turfEnvelope(segmentPolygon))
+    const segmentPolygon = turfPolygon(coordinates);
+    const envelopeBbox = turfBbox(turfEnvelope(segmentPolygon));
 
     // Most left geohash in box OR the next geohash after current rowProgress
     const startingGeohash = ngeohash.encode(
       envelopeBbox[3],
       Math.max(this.rowProgress, envelopeBbox[0] + 0.00001), // Add some small long value to avoid edge cases
-      this.options.precision
-    )
+      this.options.precision,
+    );
 
-    const geohashList = []
+    const geohashList = [];
 
     // Checking every geohash in the row from left to right
-    let currentGeohash = startingGeohash
+    let currentGeohash = startingGeohash;
 
     while (true) {
-      const geohashPolygon = turfBboxPolygon(switchBbox(ngeohash.decode_bbox(currentGeohash)))
+      const geohashPolygon = turfBboxPolygon(
+        switchBbox(ngeohash.decode_bbox(currentGeohash)),
+      );
 
-      let addGeohash = false
+      let addGeohash = false;
 
       if (this.originalShape.geometry.type === "LineString") {
         // We add every geohash because all segments that come in are envelopes of the relevant line segments
-        addGeohash = true
+        addGeohash = true;
       } else {
         // Its a polygon
         switch (this.options.hashMode) {
           case "intersect":
             // Only add geohash if they intersect/overlap with the original polygon
-            addGeohash = turfBooleanOverlap(segmentPolygon, geohashPolygon)
+            addGeohash = turfBooleanOverlap(segmentPolygon, geohashPolygon);
 
             if (addGeohash && this.minIntersectArea > 0) {
-              const intersect = turfIntersect(this.originalShape, geohashPolygon)
-              addGeohash = turfArea(intersect) >= this.minIntersectArea
+              const intersect = turfIntersect(
+                this.originalShape,
+                geohashPolygon,
+              );
+              addGeohash = turfArea(intersect) >= this.minIntersectArea;
             }
-            break
+            break;
           case "envelope":
-            addGeohash = true // add every geohash
-            break
+            addGeohash = true; // add every geohash
+            break;
           case "insideOnly":
             // Only add geohash if it is completely within the original polygon
             addGeohash =
               turfBooleanWithin(geohashPolygon, this.originalShape) &&
-              allRectangleEdgesWithin(geohashPolygon, this.originalShape)
+              allRectangleEdgesWithin(geohashPolygon, this.originalShape);
             // Extra check to avoid turf.js bug
             // REMOVE allRectangleEdgesWithin CHECK IF POSSIBLE -> NEGATIVE PERFORMANCE IMPACT
-            break
+            break;
           case "border":
             // Only add geohash if they overlap
             addGeohash =
               turfBooleanOverlap(segmentPolygon, geohashPolygon) &&
-              !turfBooleanWithin(geohashPolygon, this.originalShape)
-            break
+              !turfBooleanWithin(geohashPolygon, this.originalShape);
+            break;
         }
       }
 
@@ -294,26 +330,26 @@ class GeohashStream extends Stream.Readable {
 
       // If yes -> add it to the list of geohashes
       if (addGeohash) {
-        geohashList.push(currentGeohash)
+        geohashList.push(currentGeohash);
       }
 
       // Save rowProgress
       // maxX plus some small amount to avoid overlapping edges due to lat/long inaccuracies
-      this.rowProgress = turfBbox(geohashPolygon)[2] + 0.00001
+      this.rowProgress = turfBbox(geohashPolygon)[2] + 0.00001;
 
-      const maxX = geohashPolygon.bbox[2]
+      const maxX = geohashPolygon.bbox[2];
       if (maxX >= envelopeBbox[2]) {
         // If right edge of current geohash is out of bounds we are done
-        currentGeohash = null
-        break
+        currentGeohash = null;
+        break;
       }
 
       // Get eastern neighbor and set him as next geohash to be checked
-      currentGeohash = ngeohash.neighbor(currentGeohash, [0, 1])
+      currentGeohash = ngeohash.neighbor(currentGeohash, [0, 1]);
     }
 
-    return geohashList
+    return geohashList;
   }
 }
 
-module.exports = shape2geohash
+module.exports = shape2geohash;
